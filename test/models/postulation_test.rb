ca4cd8d12f6492792916f7ac33cd4bcc6c43bae8
Carlos Santana <carlos.santanag@live.cl>
2024-10-28 22:17:00 -0300

    v1.4 - Incorpora test unitario

--- test/models/postulation_test.rb
+++ test/models/postulation_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class PostulationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :all, except: :offers # Esto omite los datos de offers.yml y solo usa los datos creados en setup
+
+  setup do
+    @admin = User.create!(email: "owner@example.com", password: "123456", role: "owner",  phone: 987654321, name: "Esteban", curriculum: "CV Esteban")
+    @user  = User.create!(email: "carlos@example.com", password: "123456", role: "normal", phone: 987654321, name: "Carlos", curriculum: "CV Carlos")
+    @offer = Offer.create!(title: "Desarrollador de software", description: "Multifacético", active: true, limit: Date.tomorrow, user: @admin)
+  end
+
+  test "should create a postulation and notify owner" do
+    # Crear la postulación para el usuario
+    postulation = Postulation.new(user: @user, offer: @offer, message: "Estoy interesado en el puesto.")
+    assert postulation.save, "La postulación debería guardarse correctamente"
+
+    # Verificar que la postulación se asocie correctamente con el usuario y la oferta
+    assert_equal @user, postulation.user
+    assert_equal @offer, postulation.offer
+  end
 end
