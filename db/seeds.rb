6d05ca64df80fd6d4f6d6cba04431d90d2863e3f
Carlos Santana <carlos.santanag@live.cl>
2024-10-28 22:37:31 -0300

    v1.6 - Carga datos de prueba

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,52 +9,53 @@
 #   end
 
 User.create!(
-  email: "esteban@empresa.cl",
+  email: "esteban@miempresa.cl",
   password: "123456",
-  name: "Esteban Steele",
+  name: "Esteban García",
   phone: 987654321,
   role: 1,
   picture: File.open(Rails.root.join("db/seeds/esteban.jpg")))
 
+puts "La cuenta de Esteban (owner) ha sido creada: esteba@miempresa.cl con privilegios para crear usuarios y ofertas de cargos"
+
 Offer.create!(
   title: "Ayudante bodega",
   Description: "Recibir, verificar e ingresar la documentación y la mercadería que debe resguardarse en la bodega. Almacenar y controlar la mercadería que ingresa de acuerdo con el código establecido. Alistar los pedidos de los materiales de las diferentes unidades administrativas.",
+  active: true,
+  limit: Date.tomorrow,
   user_id: User.last.id)
 
 Offer.create!(
   title: "Ayudante contable",
   Description: "Dependiente de la gerencia de Administración y Finanzas. El cargo de ayudante contable tiene por función colaborar en trasladar la información de las transacciones diarias a los libros contables digitales, libro diario y libro mayor, en ausencia de libros físicos, para el registro diario de las transacciones.",
+  active: true,
+  limit: Date.tomorrow,
   user_id: User.last.id)
 
-puts "La cuenta de Esteban (owner) ha sido creada: esteban@empresa.cl con privilegios para crear usuarios y ofertas de cargos"
 puts "Se han creado 2 ofertas de cargos"
 
 User.create!(
-  email: "steve@empresa.cl",
+  email: "steve@miempresa.cl",
   password: "123456",
   name: "Steve Austin",
   phone: 987654321,
   role: 0,
   picture: File.open(Rails.root.join("db/seeds/user1.jpg")))
 
-puts "La cuenta de Steve (empleado) ha sido creada: steve@empresa.cl con 2 postulaciones "
-
 User.create!(
-  email: "linda@empresa.cl",
+  email: "linda@miempresa.cl",
   password: "123456",
   name: "Linda Hamilton",
   phone: 987654321,
   role: 0,
   picture: File.open(Rails.root.join("db/seeds/user2.jpg")))
 
-puts "La cuenta de Linda (emplaeda) ha sido creada: linda@empresa.cl con 1 postulación"
-
 User.create!(
-  email: "juan@empresa.cl",
+  email: "juan@miempresa.cl",
   password: "123456",
   name: "Juan Machuca",
   phone: 987654321,
   role: 0,
   picture: File.open(Rails.root.join("db/seeds/user3.jpg")))
 
-puts "La cuenta de Juan (empleado)ha sido creada: juan@empresa.cl sin postulaciones"
+puts "Se crearon las cuentas de Steve, Linda y Juan con el email: nombre@miempresa.cl, paswword: 123456 y sin postulaciones"
