7863ba93ab4621742b9a9349b6edcce48f1eef2f
Carlos Santana <carlos.santanag@live.cl>
2024-10-28 22:24:26 -0300

    v1.5 - README.md

--- test/models/postulation_test.rb
+++ test/models/postulation_test.rb
@@ -1,15 +1,13 @@
 require "test_helper"
 
 class PostulationTest < ActiveSupport::TestCase
-  fixtures :all, except: :offers # Esto omite los datos de offers.yml y solo usa los datos creados en setup
-
   setup do
-    @admin = User.create!(email: "owner@example.com", password: "123456", role: "owner",  phone: 987654321, name: "Esteban", curriculum: "CV Esteban")
+    @admin = User.create!(email: "owner@example.com",  password: "123456", role: "owner",  phone: 987654321, name: "Esteban", curriculum: "CV Esteban")
     @user  = User.create!(email: "carlos@example.com", password: "123456", role: "normal", phone: 987654321, name: "Carlos", curriculum: "CV Carlos")
     @offer = Offer.create!(title: "Desarrollador de software", description: "Multifacético", active: true, limit: Date.tomorrow, user: @admin)
   end
 
-  test "should create a postulation and notify owner" do
+  test "should create a postulation" do
     # Crear la postulación para el usuario
     postulation = Postulation.new(user: @user, offer: @offer, message: "Estoy interesado en el puesto.")
     assert postulation.save, "La postulación debería guardarse correctamente"
